4ef036d9641e0d52124278b012303993fb335d18
Jack <jhilscheriv@gmail.com>
2019-08-14 14:12:50 -0400

    completed deliverables

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,39 @@
 class DogsController < ApplicationController
+
+  before_action :helper_dog, only: [:show]
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def new
+
+  end
+
+  def show
+    @employees = @dog.employees
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def helper_dog
+    @dog = Dog.find(params[:id])
+  end
+
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,58 @@
 class EmployeesController < ApplicationController
+
+  before_action :helper_paper_salesmxn_and_accoutant, only: [:show, :update, :edit]
+
+  def index
+    @employees = Employee.all
+  end
+
+
+  def new
+    @employee = Employee.new
+    @dogs = Dog.all
+  end
+
+  def show
+
+  end
+
+  def create
+    @employee = Employee.create(employee_params)
+    if @employee.valid?
+      @employee.errors
+      redirect_to @employee
+    else
+      flash[:errors] = @employee.errors.full_messages
+      redirect_to new_employee_path
+    end
+  end
+
+  def update
+
+    @employee.update(employee_params)
+    if @employee.valid?
+      redirect_to @employee
+    else
+      flash[:errors] = @employee.errors.full_messages
+      redirect_to edit_employee_path(@employee)
+    end
+  end
+
+  def edit
+    @dogs = Dog.all
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def helper_paper_salesmxn_and_accoutant
+    @employee = Employee.find(params[:id])
+  end
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :alias, :office, :title, :img_url, :dog_id)
+  end
 end
